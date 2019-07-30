154ccbe7535c1bbb78954a1da831849e966eafdf
anyue <1183767577@qq.com>
2019-07-30 09:22:49 +0800

    格式化代码。

--- FSMDemo/Assets/Low_Swordman/Demo/Scripts/PlayerController.cs
+++ FSMDemo/Assets/Low_Swordman/Demo/Scripts/PlayerController.cs
@@ -9,8 +9,7 @@ public abstract class PlayerController :MonoBehaviour
     public int currentJumpCount = 0; 
     public bool isGrounded = false;
     public bool OnceJumpRayCheck = false;
-
-    public bool Is_DownJump_GroundCheck = false;   // 다운 점프를 하는데 아래 블록인지 그라운드인지 알려주는 불값
+    public bool Is_DownJump_GroundCheck = false;   
     protected float m_MoveX;
     public Rigidbody2D m_rigidbody;
     protected CapsuleCollider2D m_CapsulleCollider;
@@ -20,54 +19,33 @@ public abstract class PlayerController :MonoBehaviour
     public float MoveSpeed = 6;
     public int JumpCount = 2;
     public float jumpForce = 15f;
-
     protected void AnimUpdate()
     {
-
-
         if (!m_Anim.GetCurrentAnimatorStateInfo(0).IsName("Attack"))
         {
             if (Input.GetKey(KeyCode.Mouse0))
             {
-
-
                 m_Anim.Play("Attack");
             }
             else
             {
-
                 if (m_MoveX == 0)
                 {
                     if (!OnceJumpRayCheck)
                         m_Anim.Play("Idle");
-
                 }
                 else
                 {
-
                     m_Anim.Play("Run");
                 }
-
             }
-
-
-
         }
-
     }
-
-
-
 
     protected void Filp(bool bLeft)
     {
-
-
         transform.localScale = new Vector3(bLeft ? 1 : -1, 1, 1);
-
     }
-
-
     protected void prefromJump()
     {
         m_Anim.Play("Jump");
@@ -77,18 +55,15 @@ public abstract class PlayerController :MonoBehaviour
         m_rigidbody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
 
         OnceJumpRayCheck = true;
+
         isGrounded = false;
 
-
         currentJumpCount++;
-
     }
-
     protected void DownJump()
     {
         if (!isGrounded)
             return;
-
 
         if (!Is_DownJump_GroundCheck)
         {
@@ -100,10 +75,7 @@ public abstract class PlayerController :MonoBehaviour
             m_CapsulleCollider.enabled = false;
 
             StartCoroutine(GroundCapsulleColliderTimmerFuc());
-
         }
-
-
     }
 
     IEnumerator GroundCapsulleColliderTimmerFuc()
@@ -111,12 +83,8 @@ public abstract class PlayerController :MonoBehaviour
         yield return new WaitForSeconds(0.3f);
         m_CapsulleCollider.enabled = true;
     }
-
-
-    //////바닥 체크 레이케스트 
+    
     Vector2 RayDir = Vector2.down;
-
-
     float PretmpY;
     float GroundCheckUpdateTic = 0;
     float GroundCheckUpdateTime = 0.01f;
@@ -127,21 +95,15 @@ public abstract class PlayerController :MonoBehaviour
 
         GroundCheckUpdateTic += Time.deltaTime;
 
-
-
         if (GroundCheckUpdateTic > GroundCheckUpdateTime)
         {
             GroundCheckUpdateTic = 0;
-
-
 
             if (PretmpY == 0)
             {
                 PretmpY = transform.position.y;
                 return;
             }
-
-
 
             float reY = transform.position.y - PretmpY;  //    -1  - 0 = -1 ,  -2 -   -1 = -3
 
@@ -161,23 +123,11 @@ public abstract class PlayerController :MonoBehaviour
                     Debug.Log("안부딪힘");
 
                 }
-
-
             }
 
-
             PretmpY = transform.position.y;
-
         }
-
-
-
-
     }
 
-
-
     protected abstract void LandingEvent();
-
-
 }
